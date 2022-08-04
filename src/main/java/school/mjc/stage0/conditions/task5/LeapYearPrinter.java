e746004ec8fd38fe49f1b0bf7449a52c7f1e3402
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-04 22:43:46 +0600

    Number #1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year == 2004 || year == 2000){
+            System.out.println("leap");
+        }else {
+            System.out.println("not leap");
+        }
     }
 }
